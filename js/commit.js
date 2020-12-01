58be937396db96daa85500291d1b50326e91a105
DaisyForeve <wangchengyuaun@njsdata>
2020-12-02 00:01:14 +0800

    feat: 网础基基础代码

---
+++ js/commit.js
@@ -0,0 +1,45 @@
+$(function () {
+    // banner图
+    var bannerSwiper = new Swiper(".banner-swiper", {
+        loop: true,
+        autoplay: true,
+        speed: 1000,
+        pagination: {
+            el: '.banner-swiper .swiper-pagination',
+        },
+        // effect: 'cube',
+    })
+
+    // 新闻
+    // banner图
+    var newSwiper = new Swiper(".index_new_swiper", {
+        loop: true,
+        autoplay: true,
+        speed: 1000,
+        slidesPerView: 3,
+        navigation: {
+            nextEl: '.index_new_swiper .swiper-button-next',
+            prevEl: '.index_new_swiper .swiper-button-prev',
+        },
+        // effect: 'cube',
+    })
+
+     // 关于我们
+     var newSwiper = new Swiper(".about_info_content", {
+        loop: true,
+        autoplay: true,
+        speed: 1000,
+        slidesPerView: 3,
+        pagination: {
+            el: '.about_info_content .swiper-pagination',
+        },
+        // effect: 'cube',
+    })
+
+    var coreSwiper = new Swiper('.service_swiper', {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        centeredSlides: true,
+        loop: true,
+      });
+})
